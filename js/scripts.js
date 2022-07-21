8fc40e03f2fa7e06cd8aac5232d715447b1ca17e
AleksVZ <zaiko1984@ukr.net>
2022-07-21 14:29:51 +0300

    ass section (footer)

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,11 @@ $(document).ready(function(){
 
     $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:0,
         nav:true,
         dotsEach:true,
         smartSpeed:3000,
         margin:20,
-        
         navText: [ '', ' ' ],
-
         responsive:{
             0:{
                 items:1
@@ -26,23 +23,5 @@ $(document).ready(function(){
 
 
 
-      $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        smartSpeed:2000,
-        margin:20,
-        navText: [ '', ' ' ],
 
-        responsive:{
-            0:{
-                items:1
-            },
-            1100:{
-                items:3
-            }
-        }
-    });
-    $( function() {
-        $( "#accordion" ).accordion();
-      } );
 });
